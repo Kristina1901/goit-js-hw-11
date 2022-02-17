058863a7fb8272a4e5208e9fcd8694e0280f510c
Kristina <ktuzova1901@gmail.com>
2022-02-17 15:07:00 +0200

    button

--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ import articlesTpl from './templates/articles.hbs'
 
 const formEl = document.querySelector(".search-form")
 const input = document.querySelector('input')
-// const show = document.querySelector("button")
+const show = document.querySelector(".load-more")
 const container = document.querySelector(".gallery")
 const newsApiService = new NewApiService();
 formEl.addEventListener('submit', onSearch)    
 function onSearch(e) {
   e.preventDefault();
-    // clearArticlesContainer()
+    clearArticlesContainer()
     newsApiService.query = input.value
     newsApiService.resetPage()
     newsApiService.fetchArticles().then(appendArtticleMarkup)
    }
-// show.addEventListener('click', onLoadMore)
-// function onLoadMore() {
-//   newsApiService.fetchArticles().then(appendArtticleMarkup)
-// }
-function appendArtticleMarkup(hits) {
-    container.insertAdjacentHTML('beforeend', articlesTpl
-        (hits));
+show.addEventListener('click', onLoadMore)
+function onLoadMore() {
+  newsApiService.fetchArticles().then(appendArtticleMarkup)
 }
-// function clearArticlesContainer() {
-//   container.innerHTML = ''
-// }
+function appendArtticleMarkup(data) {
+    container.insertAdjacentHTML('beforeend', articlesTpl
+        (data));
+}
+function clearArticlesContainer() {
+  container.innerHTML = ''
+}
--- src/news-service.js
+++ src/news-service.js
@@ -1,21 +1,31 @@
 const axios = require('axios');
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 export default class NewsApiService{
     constructor() {
         this.searchQuery = '';
         this.page = 1;
       
     } 
-    fetchArticles() {
-    const url =`https://pixabay.com/api/?key=25742828-fa226770f9336c5f983da529f&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch&per_page=40&page=${this.page}`
-    return fetch(url)
-    .then(r => r.json())
-    .then(data => {
-        this.incrementPage()
-        
-        return data.hits
-        })
+    async fetchArticles() {
+        try {
+            const response = await axios.get(`https://pixabay.com/api/?key=25742828-fa226770f9336c5f983da529f&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch&per_page=40&page=${this.page}`)
+            const data = response.data.hits
+           
+            if (data.length === 0) {
+                return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+            }
+            if (data.length !== 0) {
+                this.incrementPage()
+                return data
+            }
+            
+        }
+    catch (error) {
+     console.log(error.message);
+      }
+      }    
     
-    }
+    
     incrementPage() {
         this.page += 1
     }
